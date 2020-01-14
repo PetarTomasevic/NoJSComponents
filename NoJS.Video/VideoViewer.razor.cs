287ccddd881244b62a42a0f2c2f32f4308205e7c
Petar Tomasevic <petar.tomasevic@ri-ing.net>
2020-01-14 15:03:25 +0100

    VideoViewer  component added

---
+++ NoJS.Video/VideoViewer.razor.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NoJS.Video
+{
+    public class VideoViewerBase : ComponentBase
+    {
+        [Parameter] public string VideoSource { get; set; }
+        [Parameter] public string VideoType { get; set; }
+        [Parameter] public string VideoWidth { get; set; }
+    }
+}
